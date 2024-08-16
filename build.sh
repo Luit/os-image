e00a7a03d39839dfc256a96d525b0cb26dcb2e93
Luit van Drongelen <luitvd@gmail.com>
2024-08-16 20:55:37 +0200

    add my overrides/installs

--- build.sh
+++ build.sh
@@ -2,22 +2,20 @@
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E %fedora)"
+rpm-ostree override remove \
+    gnome-classic-session \
+    gnome-classic-session-xsession \
+    gnome-shell-extension-apps-menu \
+    gnome-shell-extension-background-logo \
+    gnome-shell-extension-launch-new-instance \
+    gnome-shell-extension-places-menu \
+    gnome-shell-extension-window-list
 
-
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+rpm-ostree install \
+    distrobox \
+    firewall-config \
+    mozilla-openh264 \
+    ptyxis \
+    pulseaudio-utils \
+    steam-devices \
+    tailscale
